8b04362b502604577df7c680fae3602b193cc980
duonggiatuong <43407477+duonggiatuong@users.noreply.github.com>
2025-06-05 11:40:44 +0700

    Set up JavaScript dependencies and files

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,12 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
   server: {
     port: 5173,
-    hot: true
+    open: true
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      }
-    }
+    assetsDir: 'assets'
   }
 });
